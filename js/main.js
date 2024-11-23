b828c78f64132a7bf288d84b834e4e3947c7eba7
unknown <fasil2001.pp@gmail.com>
2024-11-23 20:04:53 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,157 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+
+
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Screenshot carousel
+    $(".screenshot-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        loop: true,
+        dots: true,
+        items: 1
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+
+
+function typingEffect() {
+    const contactTexts = shuffleArray(['Want to tell us something?😊', 'Ask us anything!😄', 'Want to talk about an idea?', 'Lets do something together!🤗', 'Do you need help in something?👍']);
+    const typedtext = document.getElementsByClassName("typedtext")[0];
+    let removing = false;
+    let idx = char = 0;
+
+    setInterval(() => { // We define the interval of the typing speed
+
+        // If we do not reach the limit, we insert characters in the html
+        if (char < contactTexts[idx].length) typedtext.innerHTML += contactTexts[idx][char];
+
+        // 15*150ms = time before starting to remove characters
+        if (char == contactTexts[idx].length + 15) removing = true;
+
+        // Removing characters, the last one always
+        if (removing) typedtext.innerHTML = typedtext.innerHTML.substring(0, typedtext.innerHTML.length - 1);
+
+        char++; // Next character
+
+        // When there is nothing else to remove
+        if (typedtext.innerHTML.length === 0) {
+
+            // If we get to the end of the texts we start over
+            if (idx === contactTexts.length - 1) idx = 0
+            else idx++;
+
+            char = 0; // Start the next text by the first character
+            removing = false; // No more removing characters
+        }
+
+    }, 150); // Typing speed, 150 ms
+
+}
+typingEffect();
+function shuffleArray(array) {
+    let currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
